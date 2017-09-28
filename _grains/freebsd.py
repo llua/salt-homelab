7551ac6efe19800f11724606c8345b37c7aba811
Eric Cook <llua@gmx.com>
2017-09-28 00:16:27 -0400

    improve freebsd_jailed grain

--- _grains/freebsd.py
+++ _grains/freebsd.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python2
 def freebsd_jailed():
   import platform
-  from subprocess import check_output
-  grains = { 'freebsd_jailed': False }
+  import salt.modules.cmdmod
+
+  __salt__ = {
+               'cmd.run_stdout': salt.modules.cmdmod.run_stdout
+             }
+    grains = { 'freebsd_jailed': False }
+
   if 'FreeBSD' in platform.uname():
-    if int(check_output('sysctl -n security.jail.jailed',shell=True)) == 1:
+    if int(__salt__['cmd.run_stdout'](
+        'sysctl -n security.jail.jailed',
+        output_loglevel='trace')) == 1:
+
       grains['freebsd_jailed'] = True
   return grains
